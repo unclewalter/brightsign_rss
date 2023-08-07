f0cf20f3871a51fdde92425d916f07b1199f8f15
Kevan Atkins <kevan.atkins@gmail.com>
2023-08-07 20:21:08 +1000

    Restructure media folder to be able to have different creator folders

--- index.js
+++ index.js
@@ -68,30 +68,32 @@ function getChecksum(path) {
 
 // ======= Feed generator =======
 
-function createRSSFeed(feed_id) {
+function createRSSFeed(creator_id, screen_id) {
 
-    const media_directory = root_media_directory + feed_id + '/';
+    const media_directory = `${root_media_directory}${creator_id}/${screen_id}/`;
+
+    console.log('media_directory: ', media_directory);
 
     fs.readdir(media_directory, (err, files) => {
         let promise_map = files.map((file) => {
             let fileDetails = fs.lstatSync(path.resolve(media_directory, file));
             // check if the file is directory 
             if (fileDetails.isDirectory()) {
-                console.log('Directory: ' + file);
+                // console.log('Directory: ' + file);
             } else {
                 item_url = media_directory + file
-                console.log('File: ' + item_url);
+                // console.log('File: ' + item_url);
                 return getChecksum(item_url);
             }
         });
 
         Promise.all(promise_map).then((value) => {
-            const feed_base_url = 'http://' + server_host + ':' + PORT + '/';
+            const feed_base_url = `http://${server_host}:${PORT}/`;
 
             let feed = new RSS({
                 title: 'NTH Screen 3 MRSS',
                 description: 'MRSS feed for Brightsign player',
-                feed_url: feed_base_url + feed_id + '_feed.xml',
+                feed_url: `${feed_base_url}${creator_id}_${screen_id}_feed.xml`,
                 site_url: feed_base_url,
                 ttl: '1',
                 custom_namespaces: {
@@ -103,7 +105,7 @@ function createRSSFeed(feed_id) {
                 const stats = fs.statSync(item.path);
                 const mime_type = mime.getType(item.path);
                 if (item && accepted_mime_types.includes(mime_type)) {
-                    console.log('item: ', item);
+                    // console.log('item: ', item);
                     item_url = encodeURI(feed_base_url + item.path.replace('/usr/share/', ''));
                     feed.item({
                         title: path.basename(item.path),
@@ -127,9 +129,9 @@ function createRSSFeed(feed_id) {
                 }
             });
 
-            fs.writeFile('public/' + feed_id + '_feed.xml', feed.xml(), function (err) {
+            fs.writeFile(`public/${creator_id}_${screen_id}_feed.xml`, feed.xml(), function (err) {
                 if (err) {
-                    return console.log(err);
+                    return console.err(err);
                 }
                 console.log("The file was saved!");
             });
@@ -142,14 +144,25 @@ function createRSSFeed(feed_id) {
 // ======= Watch /media folder =======
 
 function refreshRSSFeeds() {
-    fs.readdir(root_media_directory, (err, files) => {
-        files.forEach((file) => {
-            let fileDetails = fs.lstatSync(path.resolve(root_media_directory, file));
-            if (fileDetails.isDirectory()) {
-                console.log('Directory: ' + file);
-                createRSSFeed(file.toString());
-            }
-        })
+    fs.readdir(root_media_directory, (err, root_dir_files) => {
+        if (err) {
+            console.error(err);
+        } else {
+            root_dir_files.forEach((root_dir_file) => {
+                let root_dir_file_details = fs.lstatSync(path.resolve(root_media_directory, root_dir_file));
+                if (root_dir_file_details.isDirectory()) {
+                    let creator_directory = `${root_media_directory}${root_dir_file}`;
+                    fs.readdir(creator_directory, (err, creator_dir_files) => {
+                        creator_dir_files.forEach((creator_dir_file) => {
+                            let creator_dir_file_details = fs.lstatSync(path.resolve(creator_directory, creator_dir_file));
+                            if (creator_dir_file_details.isDirectory()) {
+                                createRSSFeed(root_dir_file.toString(), creator_dir_file.toString());
+                            }
+                        });
+                    });
+                }
+            });
+        }
     });
 }
 
@@ -164,6 +177,7 @@ fs.watch(root_media_directory, { recursive: true }, (eventType, filename) => {
 // ======= Static web server =======
 
 const express = require('express');
+const { ifError } = require('assert');
 const app = express();
 app.set('trust proxy', true)
 
